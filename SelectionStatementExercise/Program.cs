8d07cb8170afa2128b77a15b95841034f8c1aedd
Chris Gaudet <chrisgaudet9@gmail.com>
2024-07-21 19:42:57 -0500

    finished selection exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace SelectionStatementExercise
+﻿using static System.Collections.Specialized.BitVector32;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Number Guess
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine($"Pick a number from 1-1000");
+            var userInput= int.Parse( Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{favNumber} is too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{favNumber} is too high");
+            }
+            else
+            {
+                Console.WriteLine("hot diggity dog you got it!");
+            }
         }
     }
 }
